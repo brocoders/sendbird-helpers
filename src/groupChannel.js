aa9eb8ffd065769508744b03e894bdf05540ab97
Alexandr Golovchuk <a.golovchuk@gmail.com>
2019-01-04 17:28:53 +0200

    group Channel

--- src/groupChannel.js
+++ src/groupChannel.js
@@ -1,5 +1,8 @@
 /* @flow */
-import type { GroupChannel } from 'sendbird';
+import type {
+  GroupChannel,
+  GroupChannelListQuery,
+} from 'sendbird';
 import { sendBirdInstance } from './instance';
 
 export function channelList(limit: number = 30): Promise<Array<GroupChannel>> {
@@ -13,3 +16,29 @@ export function channelList(limit: number = 30): Promise<Array<GroupChannel>> {
     });
   });
 }
+
+export function sbCreateGroupChannelListQuery(): GroupChannelListQuery {
+  const sb = sendBirdInstance();
+  return sb.GroupChannel.createMyGroupChannelListQuery();
+}
+
+export const sbGetGroupChannelList = (groupChannelListQuery: GroupChannelListQuery): Promise<GroupChannel[]> => new Promise((resolve, reject) => {
+  groupChannelListQuery.next((channels, error) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(channels);
+    }
+  });
+});
+
+export const sbGetGroupChannel = (channelUrl: string): Promise<GroupChannel> => new Promise((resolve, reject) => {
+  const sb = sendBirdInstance();
+  sb.GroupChannel.getChannel(channelUrl, (channel: GroupChannel, error) => {
+    if (error) {
+      reject(error);
+    } else {
+      resolve(channel);
+    }
+  });
+});
