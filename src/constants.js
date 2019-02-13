2addde625a0f6c65f216942c525c97d072cabe9a
Alexandr Golovchuk <a.golovchuk@gmail.com>
2019-02-13 10:38:22 +0200

    add constants

--- src/constants.js
+++ src/constants.js
@@ -1,2 +1,7 @@
 /* @flow */
 export const DOCUMENT_CHAT_TYPE = 'document_p2p';
+export const RECONNECTING = 'RECONNECTING';
+export const CONNECTED = 'CONNECTED';
+export const FAILED = 'FAILED';
+
+export type ConnectionStatusType = 'RECONNECTING' | 'CONNECTED' | 'FAILED';
